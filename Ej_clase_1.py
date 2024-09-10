ad409650920c3ee4ba62366c369006a77ca69a10
Ernesto Alexander <164534106+Ernesto-Larrondo-1070@users.noreply.github.com>
2024-09-10 12:11:51 -0600

    Add files via upload

---
+++ Ej_clase_1.py
@@ -0,0 +1,13 @@
+print("introduccion a clases")
+class Animal:
+    edad=3
+    raza="chihuahua"
+    comida="croquetas"
+    def come(self):
+        print(f"El perro come "+self.comida)
+print(Animal)
+print("Creando el objeto de la clase Animal")
+perro = Animal()
+print(f"edad del perro {perro.edad} ")
+print(f"Raza del perro {perro.raza} ")
+perro.come()
